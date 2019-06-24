8324addf7c06f0c9c70a6c26caa243c30117d81b
zeta1178 <michaelcruz@aim.com>
2019-06-24 17:14:14 -0400

    Add files via upload

---
+++ AWS-IdentifyServer-Output.ps1
@@ -0,0 +1,13 @@
+﻿#Param (
+    #[string]$instanceID
+#)
+
+Set-AWSCredentials default
+Set-DefaultAWSRegion us-gov-west-1
+
+$destPath =  (Get-SSMParameter -Name /PowerShell/IdentifyServer/varDest).value
+$compname = (Get-SSMParameter -Name /PowerShell/IdentifyServer/varCompName).value
+$OutputContent =  (Get-SSMParameter -Name /PowerShell/IdentifyServer/OutputContent).value
+$compname2 = (get-childitem $compname).value
+New-Item $destPath -type file -force 
+Add-Content $destPath $OutputContent`$compname2
